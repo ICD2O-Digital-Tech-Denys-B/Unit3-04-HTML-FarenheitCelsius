4e6468384758ec11d14ec99f0bf57bcb464ccb9c
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-04-30 14:17:33 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function calcCelsius() {
+    let f = parseFloat(document.getElementById("F").value)
+    let step1 = f-32
+    let step2 = 5/9
+    let step3 = step1*step2
+    let answer=step3.toFixed(2)
+    document.getElementById("answer").innerHTML = answer +"°C";
+}
